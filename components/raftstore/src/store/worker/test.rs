6187e9a16b5f2bb71a553f113e69a13372ce9bee
5kbpers <tangminghua@pingcap.com>
2021-06-08 19:53:52 +0800

    push and pop
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- components/raftstore/src/store/worker/test.rs
+++ components/raftstore/src/store/worker/test.rs
@@ -31,7 +31,7 @@ impl<E: KvEngine> HoldSnapshotRunner<E> {
 
     pub fn new_timer(&self) -> Timer<()> {
         let mut timer = Timer::new(1);
-        timer.add_task(time::Duration::from_secs(30), ());
+        timer.add_task(time::Duration::from_secs(10), ());
         timer
     }
 }
@@ -40,21 +40,38 @@ impl<E: KvEngine> Runnable<Task> for HoldSnapshotRunner<E> {
     fn run(&mut self, _: Task) {}
 }
 
+// impl<E: KvEngine> RunnableWithTimer<Task, ()> for HoldSnapshotRunner<E> {
+//     fn on_timeout(&mut self, timer: &mut Timer<()>, _: ()) {
+//         let snapshots = (0..25000)
+//             .map(|_| self.engine.snapshot())
+//             .collect::<Vec<_>>();
+//         let iterators = snapshots
+//             .iter()
+//             .map(|s| {
+//                 let mut iter = s.iterator().unwrap();
+//                 iter.seek(SeekKey::Start).unwrap();
+//                 iter
+//             })
+//             .collect::<Vec<_>>();
+//         let _ = std::mem::replace(&mut self.snapshots, snapshots);
+//         let _ = std::mem::replace(&mut self.iterators, iterators);
+//         timer.add_task(time::Duration::from_secs(40), ());
+//     }
+// }
+
 impl<E: KvEngine> RunnableWithTimer<Task, ()> for HoldSnapshotRunner<E> {
-    fn on_timeout(&mut self, timer: &mut Timer<()>, _: ()) {
-        let snapshots = (0..25000)
-            .map(|_| self.engine.snapshot())
-            .collect::<Vec<_>>();
-        let iterators = snapshots
-            .iter()
-            .map(|s| {
-                let mut iter = s.iterator().unwrap();
-                iter.seek(SeekKey::Start).unwrap();
-                iter
-            })
-            .collect::<Vec<_>>();
-        let _ = std::mem::replace(&mut self.snapshots, snapshots);
-        let _ = std::mem::replace(&mut self.iterators, iterators);
-        timer.add_task(time::Duration::from_secs(40), ());
+    fn on_timeout(&mut self, _: &mut Timer<()>, _: ()) {
+        loop {
+            let snap = self.engine.snapshot();
+            let mut iter = snap.iterator().unwrap();
+            iter.seek(SeekKey::Start).unwrap();
+            if self.snapshots.len() == 3000 {
+                let _ = self.snapshots.pop();
+                let _ = self.iterators.pop();
+            }
+            self.snapshots.push(snap);
+            self.iterators.push(iter);
+            std::thread::sleep(time::Duration::from_millis(1));
+        }
     }
 }
